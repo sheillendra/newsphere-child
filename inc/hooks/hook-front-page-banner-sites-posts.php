333ad93a980b736ba9e29992e3f6c5cc9f244184
sheillendra <suryana869@gmail.com>
2024-02-16 11:03:31 +0800

    short title in featured

--- inc/hooks/hook-front-page-banner-sites-posts.php
+++ inc/hooks/hook-front-page-banner-sites-posts.php
@@ -72,7 +72,7 @@ if (!function_exists('newsphere_banner_sites_posts')) :
                                             </div>
                                             <div class="read-title">
                                                 <h4>
-                                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                                    <a href="<?php the_permalink(); ?>"><?php echo wp_trim_words( the_title(false), 5, '...' ); ?></a>
                                                 </h4>
                                             </div>
                                             <div class="entry-meta">
